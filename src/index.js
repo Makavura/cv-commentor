2669409134348da432115f65e2436dfaada38eb7
Makavura <makavuramughanga@gmail.com>
2021-01-11 12:37:28 +0300

    chore: update which component is to be loaded by react as parent

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CVCommentor from './cv-commentor';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CVCommentor></CVCommentor>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
