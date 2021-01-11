a126e2afb54d75d15c36c90e35fbda50d042ad72
Makavura <makavuramughanga@gmail.com>
2021-01-11 10:41:21 +0300

    chore: setup app entry point

---
+++ src/cv-commentor.js
@@ -0,0 +1,43 @@
+import smith from './smith.png';
+import  ListCandidates from './components/list-candidates';
+
+function App() {
+  return (
+    <div className="rc_main">
+
+      <header className={styles.rc_header}>
+
+        <div className={styles.rc_banner_txt}>
+          <p> Recruit Commentor</p>
+        </div>
+
+      </header>
+
+      <div className="rc-responsive">
+
+        <div className="rc-candidates_list">
+          <ListCandidates></ListCandidates>
+        </div>
+
+        <div id="rc_candidate_cv_view">
+
+        </div>
+
+        <div id="rc_candidate_cv">
+
+        </div>
+
+        <div id="rc_comments_view">
+          
+        </div>
+
+        <div id="rc-comments-response">
+
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
