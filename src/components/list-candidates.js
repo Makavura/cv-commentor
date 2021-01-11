4d3d5120197d3ec4f9bf4aa2a8305ffc83ef6670
Makavura <makavuramughanga@gmail.com>
2021-01-11 10:40:59 +0300

    chore: rename for ease of understanding

--- src/components/list-candidates.js
+++ src/components/list-candidates.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class ListRecruits extends React.Component {
+export default class ListCandidates extends React.Component {
     state = {
         recruits
     }
